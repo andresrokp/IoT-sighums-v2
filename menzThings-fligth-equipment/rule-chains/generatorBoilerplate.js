3e4e3d6088e1325fefa1dd35172de2a9448f6c83
andresropk <afrokap@gmail.com>
2023-02-27 20:51:04 -0500

    chore: data mock in generator

---
+++ menzThings-fligth-equipment/rule-chains/generatorBoilerplate.js
@@ -0,0 +1,12 @@
+var msgType = "POST_TELEMETRY_REQUEST";
+return { 
+    msg: {
+        "values":{
+            "sta":1677547013070,
+            "airlineIcao": "MIA",
+            "cityIcao":"BOG"
+        }
+    },
+    metadata: {},
+    msgType: msgType
+};
