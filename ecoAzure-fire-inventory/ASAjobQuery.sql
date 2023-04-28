fa3a91940b1dccf5197154b9a8da2b0e677e00c1
andresropk <afrokap@gmail.com>
2023-04-28 17:33:42 -0500

    chore: comments for shared ASA job

--- ecoAzure-fire-inventory/ASAjobQuery.sql
+++ ecoAzure-fire-inventory/ASAjobQuery.sql
@@ -1,17 +1,37 @@
+/* Por qué borraron el query enterior?
+No lo tiene que hacer muchachos,
+ellos trabajan en paralelo.*/
+
+/*RETO 17*/
+
 SELECT
     *
 INTO
     BlobEquiposMoviles
 FROM
-    ambulancia357
+    iothubreto18
 WHERE
-    GetMetadatapropertyValue(ambulancia357, 'IoTHub.ConnectionDeviceId') = 'Ambulancia_HTX357';
+    GetMetadatapropertyValue(iothubreto18, 'IoTHub.ConnectionDeviceId') = 'Ambulancia_HTX357';
 
 SELECT
     *
 INTO
     BlobBodegasFijas
 FROM
-    bodegalisamallanito
+    iothubreto18
 WHERE
-    GetMetadatapropertyValue(bodegalisamallanito, 'IoTHub.ConnectionDeviceId') = 'GMA-CENTRO-LISAMA-LLANITO-SUBSUELO';
+    GetMetadatapropertyValue(iothubreto18, 'IoTHub.ConnectionDeviceId') = 'GMA-CENTRO-LISAMA-LLANITO-SUBSUELO';
+
+/*
+TODO:
+save data as .csv to synaps input
+*/
+
+
+
+/*RETO 18*/
+
+SELECT * 
+INTO Eventhub7545
+FROM iothubreto18
+WHERE ApplicationUri='urn:GCHEAVIP21P:AspenTech:InfoPlus21:UA:Server'
