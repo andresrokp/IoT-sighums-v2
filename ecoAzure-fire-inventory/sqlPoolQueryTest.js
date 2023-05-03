6001c243942e19e70cd6d83063e44e4428edbecb
andresropk <afrokap@gmail.com>
2023-05-03 16:12:49 -0500

    feat: SQL data fetch from Pool :)

--- ecoAzure-fire-inventory/sqlPoolQueryTest.js
+++ ecoAzure-fire-inventory/sqlPoolQueryTest.js
@@ -1,1 +1,26 @@
 const sql = require('mssql');
+const { dbinfo } = require('../myvars.js')
+
+const config = {
+    server : dbinfo.server,
+    database : dbinfo.database,
+    user : dbinfo.user,
+    password : dbinfo.password,
+    options : {
+        encrypt : true,
+    }
+}
+
+async function getData(){
+    try {
+        await sql.connect(config)
+        let resp = await sql.query('SELECT * FROM dbo.ogisDataCut')
+        console.log(resp)
+    } catch (error) {
+        console.log(error)
+    } finally {
+        sql.close()
+    }
+}
+
+getData()
