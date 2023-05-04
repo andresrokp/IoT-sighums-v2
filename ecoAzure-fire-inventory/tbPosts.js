4cd6d0df8b4224bbf92f294126ee5576fdd0f256
andresropk <afrokap@gmail.com>
2023-05-03 20:07:32 -0500

    feat: can send post to ecopetrol rulechain

--- ecoAzure-fire-inventory/tbPosts.js
+++ ecoAzure-fire-inventory/tbPosts.js
@@ -1,11 +1,9 @@
 const log = console.log;
 
-
-const { now } = require('tarn/dist/utils');
 let {credentials, ecopetrolGateToken} = require('../myvars')
 
 // Pause message Telemetry to the specified device ID
-async function sendMessageToDevice(deviceId, msg) {
+async function sendMessageToDevice(msg) {
   // set variables
   const url = `https://${credentials.dns}/api/v1/${ecopetrolGateToken}/telemetry`;
   const headers = {
@@ -15,9 +13,9 @@ async function sendMessageToDevice(deviceId, msg) {
   // try the post
   try {
     const response = await fetch(url, { method: 'POST', headers, body });
-    log(`Message sent to device ${msg.gateId}`); // response
+    log(`Message sent to device ${msg.values.gateId}`); // response
   } catch (error) {
-    console.error(`Error sending message to device ${msg.gateId}:`, error);
+    console.error(`Error sending message to device ${msg.values.gateId}:`, error);
   }
 }
 
@@ -25,16 +23,21 @@ let start = performance.now()
 
 function gateMsgGenerator(gateId, latitude, longitude,  tagsArray){
     return{
-        ts: new Date().getMilliseconds,
-        gateId,
-        latitude,
-        longitude, 
-        tagsArray
+        ts: new Date().getTime(),
+        values:{
+            ts: new Date().getTime(),
+            gateId,
+            latitude,
+            longitude, 
+            tagsArray
+        }
     }
 }
 
-gateMsgGenerator(1,11,1111,[['1tag1','1rssi1']])
+let msgGate1 = gateMsgGenerator(1,11,1111,[['1tag1','1rssi1']])
+let msgGate2 = gateMsgGenerator(2,2,2222,[['2tag1','2rssi1'],['2tag2','2rssi2']])
+let msgGate3 = gateMsgGenerator(3,33,3333,[['3tag1','3rssi1'],['3tag2','3rssi2'],['3tag3','3rssi3']])
 
-sendMessageToDevice(msg)
+sendMessageToDevice(msgGate1)
 
 log(performance.now() - start)
