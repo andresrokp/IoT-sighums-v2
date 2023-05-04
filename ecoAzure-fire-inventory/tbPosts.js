bcc516839da80b57c29ba866058464904cff891b
andresropk <afrokap@gmail.com>
2023-05-03 19:27:45 -0500

    chore: gate msg generator function

--- ecoAzure-fire-inventory/tbPosts.js
+++ ecoAzure-fire-inventory/tbPosts.js
@@ -1,26 +1,40 @@
 const log = console.log;
 
 
+const { now } = require('tarn/dist/utils');
 let {credentials, ecopetrolGateToken} = require('../myvars')
 
 // Pause message Telemetry to the specified device ID
-async function sendMessageToDevice(deviceId, message) {
+async function sendMessageToDevice(deviceId, msg) {
   // set variables
   const url = `https://${credentials.dns}/api/v1/${ecopetrolGateToken}/telemetry`;
   const headers = {
     'Content-Type': 'application/json'
   };
-  const body = JSON.stringify(message);
+  const body = JSON.stringify(msg);
   // try the post
   try {
     const response = await fetch(url, { method: 'POST', headers, body });
-    log(`Message sent to device ${message.device.uniqueId} - ${message.device.name}`); // response
+    log(`Message sent to device ${msg.gateId}`); // response
   } catch (error) {
-    console.error(`Error sending message to device ${deviceId}:`, error);
+    console.error(`Error sending message to device ${msg.gateId}:`, error);
   }
 }
 
 let start = performance.now()
 
+function gateMsgGenerator(gateId, latitude, longitude,  tagsArray){
+    return{
+        ts: new Date().getMilliseconds,
+        gateId,
+        latitude,
+        longitude, 
+        tagsArray
+    }
+}
+
+gateMsgGenerator(1,11,1111,[['1tag1','1rssi1']])
+
+sendMessageToDevice(msg)
 
 log(performance.now() - start)
