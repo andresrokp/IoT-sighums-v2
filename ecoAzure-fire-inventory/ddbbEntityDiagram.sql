056e58fbb83748402fd1cc290df984bbb8ca4007
andresropk <afrokap@gmail.com>
2023-05-16 12:12:53 -0500

    refactor: rename columns | delete unused

--- ecoAzure-fire-inventory/ddbbEntityDiagram.sql
+++ ecoAzure-fire-inventory/ddbbEntityDiagram.sql
@@ -1,10 +1,8 @@
 -- // Use DBML to define your database structure
 -- // Docs: https://dbml.dbdiagram.io/docs
 
--- // Ref: users.id < follows.following_user_id
-
 Table REGISTROS {
-  ts timestamp
+  ts timestamp [primary key]
   tag_id varchar
   gate_id varchar
   longitude float
@@ -19,26 +17,26 @@ Table LINK_REG_TO_REC {
 Table RECURSOS {
   recurso_id integer [primary key]
   nombre varchar
-  especificaciones text
+  especificaciones_equipo text
   estado_teorico varchar
   fabricante varchar
   unidad varchar
   capacidad integer
-  cod_inventario integer
+  codigo_inventario integer
   sap_id integer
   ogis_id integer
-  assigned_bodega_id varchar
+  bodega_id varchar
   codigo_pec varchar
   expediente_lam varchar
-  base_id varchar
+  base_respuesta_id varchar
   actividad_aplicacion varchar
   observaciones varchar
 }
 
 Table BODEGAS {
-  gate_id varchar [primary key]
-  nombre varchar
-  tipo varchar
+  bodega_id varchar [primary key]
+  nombre_bodega varchar
+  tipo_bodega varchar
   facilidad_sistema_transporte varchar
   instalacion varchar
   vicepresidencia varchar
@@ -46,31 +44,16 @@ Table BODEGAS {
 }
 
 Table BASES {
-  base_id varchar [primary key]
-  nombre varchar
+  base_respuesta_id varchar [primary key]
+  nombre_base_respuesta varchar
+  nombre_funcionario_contacto varchar
   funcionario_contacto varchar
   telefono_contacto varchar
   correo_contacto varchar
 }
 
-Table SAP_INFO{
-  sap_id integer
-  dato_sap_1 varchar
-  dato_sap_2 varchar
-  dato_sap_3 varchar
-}
-
-Table OGIS_INFO{
-  ogis_id integer
-  dato_ogis_1 varchar
-  dato_osgi_2 varchar
-  dato_ogis_3 varchar
-}
-
 Ref: REGISTROS.tag_id > LINK_REG_TO_REC.tag_id
 Ref: LINK_REG_TO_REC.recurso_id > RECURSOS.recurso_id
-Ref: REGISTROS.gate_id > BODEGAS.gate_id
-Ref: RECURSOS.sap_id > SAP_INFO.sap_id
-Ref: RECURSOS.ogis_id > OGIS_INFO.ogis_id
-Ref: RECURSOS.assigned_bodega_id > BODEGAS.gate_id
-Ref: RECURSOS.base_id > BASES.base_id
+Ref: REGISTROS.gate_id > BODEGAS.bodega_id
+Ref: RECURSOS.bodega_id > BODEGAS.bodega_id
+Ref: RECURSOS.base_respuesta_id > BASES.base_respuesta_id
