1d4b6794a2b8afe3b5f5dd344cd52d5b5631260e
andresropk <afrokap@gmail.com>
2023-05-16 19:13:44 -0500

    feat: bodega table and mock

--- ecoAzure-fire-inventory/ssmsSqlPoolScript.sql
+++ ecoAzure-fire-inventory/ssmsSqlPoolScript.sql
@@ -1,32 +1,44 @@
 --crete the schema to the dbo user
 CREATE SCHEMA contraincendiosh
 AUTHORIZATION dbo
+GO
 
 -- Drop the table if it already exists
 IF OBJECT_ID('contraincendiosh.bodegas', 'U') IS NOT NULL
 DROP TABLE rci.bodegas
 GO
 
--- Create bodegas in rci
-CREATE TABLE contraincendiosh.bodegas
-(
-	id INT IDENTITY(1,1),
-	name NVARCHAR(50) NOT NULL,
-	latitude DECIMAL(10,7) NOT NULL,
-	longitude DECIMAL(10,7) NOT NULL,
-	tipo NVARCHAR(50) NOT NULL
-);
+
+--Drop bodegas
+IF OBJECT_ID('contraincendiosh.bodegas', 'U') IS NOT NULL
+DROP TABLE contraincendiosh.BODEGAS
+GO
+-- Create bodegas
+CREATE TABLE contraincendiosh.BODEGAS (
+  [bodega_id] nvarchar(255),
+  [nombre_bodega] nvarchar(255),
+  [tipo_bodega] nvarchar(255),
+  [facilidad_sistema_transporte] nvarchar(255),
+  [instalacion] nvarchar(255),
+  [vicepresidencia] nvarchar(255),
+  [gerencia_dpto] nvarchar(255)
+)
 GO
 
+--Fill bodegas
+INSERT INTO contraincendiosh.BODEGAS (bodega_id, nombre_bodega, tipo_bodega, facilidad_sistema_transporte, instalacion, vicepresidencia, gerencia_dpto)
+select 'bodega_id_2', 'nombre_bodega_2', 'tipo_bodega_2', 'facilidad_sistema_transporte_2', 'instalacion_2', 'vicepresidencia_2', 'gerencia_2' union
+select 'bodega_id_3', 'nombre_bodega_3', 'tipo_bodega_3', 'facilidad_sistema_transporte_3', 'instalacion_3', 'vicepresidencia_3', 'gerencia_3' union
+select 'bodega_id_4', 'nombre_bodega_4', 'tipo_bodega_4', 'facilidad_sistema_transporte_4', 'instalacion_4', 'vicepresidencia_4', 'gerencia_4' union
+select 'bodega_id_5', 'nombre_bodega_5', 'tipo_bodega_5', 'facilidad_sistema_transporte_5', 'instalacion_5', 'vicepresidencia_5', 'gerencia_5'
+
+
+
+
+SELECT * FROM contraincendiosh.bodegas
+GO
 
 --Creation of test OGIS DDBB
-
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE TABLE [dbo].[ogisDataCut]
 (
 	[ID] [smallint] NULL,
@@ -58,18 +70,9 @@ CREATE TABLE [dbo].[ogisDataCut]
 	[TIPO_ELEMENTO] [varchar](46) NULL,
 	[RUTA_ACCESO] [varchar](55) NULL
 )
-WITH
-(
-	DISTRIBUTION = ROUND_ROBIN,
-	CLUSTERED COLUMNSTORE INDEX
-)
-GO
 
 SELECT * FROM dbo.ogisDataCut
 
-
-INSERT INTO contraincendiosh.bodegas (name,latitude,longitude,tipo) VALUES ('Bodega C.I. Yondo','7.003304','-73.911276','fija');
-SELECT * FROM contraincendiosh.bodegas;
 
 
 CREATE TABLE contraincendiosh.registros
@@ -82,5 +85,7 @@ CREATE TABLE contraincendiosh.registros
     tag varchar(50) NOT NULL,
     rssi decimal(10, 2) NOT NULL
 );
+GO
 
 SELECT * FROM contraincendiosh.registros
+GO
